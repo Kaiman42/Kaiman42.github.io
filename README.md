5b83572f6b1479df5f69aa0fe1ecea2d47682036
Kaiman42 <135389100+Kaiman42@users.noreply.github.com>
2023-08-16 09:37:39 -0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# kaiman42.github.io
+# Acervo de sites Kaiman42
+Esse site reúne uma lista de outros sites de variadas utilidades que recolhi durante o uso do navegador.
  
--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static('public'));
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
 
 const form = document.querySelector('form');
 
@@ -14,7 +5,6 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   
   const query = form.querySelector('input').value;
-  
-  // faz a pesquisa aqui
+
   search(query); 
 })
