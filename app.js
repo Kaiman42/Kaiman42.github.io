d34c0adfc4eac2f1190586e95ce7f696c1a77081
Kaiman42 <135389100+Kaiman42@users.noreply.github.com>
2023-08-16 08:33:25 -0300

    Barra de pesquisa

--- app.js
+++ app.js
@@ -7,3 +7,14 @@ app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  const query = form.querySelector('input').value;
+  
+  // faz a pesquisa aqui
+  search(query); 
+})
