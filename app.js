f0a5a323fe2b329bb33ef1dcf284dbc2774d720c
Kaiman42 <135389100+Kaiman42@users.noreply.github.com>
2023-11-12 15:03:39 -0300

    att

--- app.js
+++ app.js
@@ -1,11 +1,62 @@
+//Sortear sites de A-Z
+fetch('sites.json')
+  .then(response => response.json())
+  .then(data => {
 
-/* const form = document.querySelector('form');
+    const sites = data;
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  
-  const query = form.querySelector('input').value;
+    sites.sort((a, b) => {
+      const siteA = a.name.toUpperCase();
+      const siteB = b.name.toUpperCase();
 
-  search(query); 
-}) */
+      if (siteA < siteB) {
+        return -1;
+      }
 
+      if (siteA > siteB) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    //Criar lista de sites usando .json
+    sites.forEach(site => {
+
+      const listItem = document.createElement('li');
+
+      const siteName = document.createElement('h3');
+      siteName.textContent = site.name;
+      siteName.addEventListener('click', function() {
+        window.location.href = site.url;
+      });
+
+      const siteDesc = document.createElement('p');
+      siteDesc.textContent = site.desc;
+      siteDesc.addEventListener('click', function() {
+        window.location.href = site.url;
+      });
+
+      listItem.appendChild(siteName);
+      listItem.appendChild(siteDesc);
+
+      sitesList.appendChild(listItem);
+      
+    });
+
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+  //Fechar cabeçalho
+  window.onload = function() {
+
+    var header = document.getElementsByClassName('header');
+    var button = document.getElementById('closeBtn');
+
+    button.addEventListener('click', function() {
+        
+        header.classList.add('closed');
+    });
+};
