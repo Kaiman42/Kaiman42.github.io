5942ca42293d1a0a8e71680ab957a75cbfd261d9
Kaiman42 <135389100+Kaiman42@users.noreply.github.com>
2023-11-13 18:47:16 -0300

    pesquisa

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-//Sortear sites de A-Z
+const sitesList = document.getElementById('sitesList');
+const filteredList = document.getElementById('filteredList');
+const searchInput = document.getElementById('searchInput');
+const form = document.querySelector('form');
+
+function createListItem(site, list) {
+  const listItem = document.createElement('li');
+
+  const siteName = document.createElement('h3');
+  siteName.textContent = site.name;
+  siteName.addEventListener('click', function() {
+    window.location.href = site.url;
+  });
+
+  const siteDesc = document.createElement('p');
+  siteDesc.textContent = site.desc;
+  siteDesc.addEventListener('click', function() {
+    window.location.href = site.url;
+  });
+
+  listItem.appendChild(siteName);
+  listItem.appendChild(siteDesc);
+
+  list.appendChild(listItem);
+}
+
 fetch('sites.json')
   .then(response => response.json())
   .then(data => {
-
     const sites = data;
-
     sites.sort((a, b) => {
       const siteA = a.name.toUpperCase();
       const siteB = b.name.toUpperCase();
@@ -20,34 +43,40 @@ fetch('sites.json')
       return 0;
     });
 
-    //Criar lista de sites usando .json
     sites.forEach(site => {
-
-      const listItem = document.createElement('li');
-
-      const siteName = document.createElement('h3');
-      siteName.textContent = site.name;
-      siteName.addEventListener('click', function() {
-        window.location.href = site.url;
-      });
-
-      const siteDesc = document.createElement('p');
-      siteDesc.textContent = site.desc;
-      siteDesc.addEventListener('click', function() {
-        window.location.href = site.url;
-      });
-
-      listItem.appendChild(siteName);
-      listItem.appendChild(siteDesc);
-
-      sitesList.appendChild(listItem);
-      
+      createListItem(site, sitesList);
     });
+
+    searchInput.addEventListener('input', function() {
+      const searchTerm = searchInput.value.toLowerCase();
+    
+      const filtered = sites.filter(site => site.name.toLowerCase().startsWith(searchTerm) && /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-\s]+$/g.test(site.name));
+    
+      while (sitesList.firstChild) {
+        sitesList.removeChild(sitesList.firstChild);
+      }
+    
+      while (filteredList.firstChild) {
+        filteredList.removeChild(filteredList.firstChild);
+      }
+    
+      if (searchTerm === '') {
+        sites.forEach(site => {
+          createListItem(site, sitesList);
+        });
+      } else {
+        filtered.forEach(site => {
+          createListItem(site, sitesList);
+        });
+      }
+    });
+    
 
   })
   .catch(error => {
     console.error(error);
   });
+
 
  /*  //Fechar cabeçalho
   window.onload = function() {
